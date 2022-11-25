7c0201f73b0a3b3e67cf5566cf4047afab8f4df6
DanielLampa <jonaxxmontefalco7@gmail.com>
2022-11-25 18:22:39 +0800

    Update profile.php

--- food-order/profile.php
+++ food-order/profile.php
@@ -4,7 +4,12 @@ include_once "config/db_connect.php";
 $sql = "SELECT * FROM tbl_user";
 $user= $conn->prepare($sql);
 $user->execute();
-$row=$user->fetch(PDO::FETCH_ASSOC);
+$row=$user->fetch();
+
+$fullName=$row['fname'];
+$uname=$row['unames'];
+$email=$row['email'];
+
 
 
 
@@ -19,18 +24,60 @@ if (isset($_SESSION['id']) && isset($_SESSION['fname'])) {?>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <div class="d-flex justify-content-center align-items-center vh-100">
-    	
-    	<div class="shadow w-500 p-3 text-center bg-white">
-            <?php 
-                
-                
-            ?>
-            <h3 class="display-4 ">Hello, <?=$_SESSION['fname']?></h3>
-           
-            <a href="logout.php" class="btn btn-warning">Logout</a>
-		</div>
-    </div>
+<div class="container-xl px-4 mt-4">
+    <!-- Account page navigation-->
+    <nav class="nav nav-borders">
+        <a class="nav-link active ms-0 text-warning" href="index.php" >Back to Home</a>
+        
+    </nav>
+    <hr class="mt-0 mb-4">
+    <div class="row">
+        <div class="col-xl-4">
+            <!-- Profile picture card-->
+            <div class="card mb-4 mb-xl-0">
+                <div class="card-header">Profile Picture</div>
+                <div class="card-body text-center">
+                    <!-- Profile picture image-->
+                    <img class="img-account-profile rounded-circle mb-2" src="http://bootdey.com/img/Content/avatar/avatar1.png" alt="">
+                    <!-- Profile picture help block-->
+                    <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+                    <!-- Profile picture upload button-->
+                    <button class="btn btn-primary" type="button">Upload new image</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-8">
+            <!-- Account details card-->
+            <div class="card mb-4">
+                <div class="card-header">Account Details</div>
+                <div class="card-body">
+                    <form>
+                        <!-- Form Group (username)-->
+                        <div class="mb-1">
+                            <label class="h3 mb-1">Username: <?=$uname?></label>
+                        </div>
+                        <!-- Form Row-->
+                        <div class="row gx-3 mb-3">
+                            <!-- Form Group (full name)-->
+                            <div class="col-md-6">
+                                <label class="h3 mb-1">Full name: <?=$fullName?></label>
+                                
+                            </div>
+                        
+                        <!-- Form Group (email address)-->
+                        <div class="mb-3">
+                            <label class="h3 mb-1">Email address: <?=$email?></label>
+                        </div>
+                        <!-- Save changes button-->
+                        <button class="btn btn-primary bt1 mb-1" type="button">Edit Profile</button>
+                        <button class="btn btn-success bt1"  type="button">LOG OUT</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        </div>
+</div>
+
 </body>
 </html>
 

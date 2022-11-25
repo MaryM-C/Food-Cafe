aad0be8fd551503f93952724c690eb1b1f3af2fe
DanielLampa <jonaxxmontefalco7@gmail.com>
2022-11-25 17:42:22 +0800

    Update profile.php

--- food-order/profile.php
+++ food-order/profile.php
@@ -1,8 +1,14 @@
 <?php 
 session_start();
+include_once "config/db_connect.php";
+$sql = "SELECT * FROM tbl_user";
+$user= $conn->prepare($sql);
+$user->execute();
+$row=$user->fetch(PDO::FETCH_ASSOC);
 
-if (isset($_SESSION['id']) && isset($_SESSION['fname'])) {
- ?>
+
+
+if (isset($_SESSION['id']) && isset($_SESSION['fname'])) {?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,14 +22,20 @@ if (isset($_SESSION['id']) && isset($_SESSION['fname'])) {
     <div class="d-flex justify-content-center align-items-center vh-100">
     	
     	<div class="shadow w-500 p-3 text-center bg-white">
+            <?php 
+                
+                
+            ?>
             <h3 class="display-4 ">Hello, <?=$_SESSION['fname']?></h3>
+           
             <a href="logout.php" class="btn btn-warning">Logout</a>
 		</div>
     </div>
 </body>
 </html>
 
-<?php }else {
+<?php }
+
+else {
 	header("Location: login.php");
-	exit;
-} ?>
+	exit;} ?>

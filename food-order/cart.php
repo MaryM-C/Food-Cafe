496c48f2d8185937a1732d6e1342bececca14958
DanielLampa <jonaxxmontefalco7@gmail.com>
2022-11-25 21:52:59 +0800

    --

--- food-order/cart.php
+++ food-order/cart.php
@@ -1,3 +1,10 @@
+<?php 
+    session_start();
+    include_once "config/db_connect.php";
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,29 +16,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-            <nav class="navbar navbar-expand-lg navbar-light ">
-                <a class="navbar-brand text-warning">My Cart</a>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link text-warning" href="./index.php">Home<span class="sr-only">(current)</span></a>
-                    </li>
-                    </ul>
-                    
-                </div>
-            </nav>
-
-
-        <table class="table">
-        <thead class="thead-dark">
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">Product</th>
-            <th scope="col">Price</th>
-            <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        </table>
-
+           
 </body>
 </html>
--- food-order/profile.php
+++ food-order/profile.php
@@ -21,7 +21,7 @@ if($user->rowCount() == 1){
 
 
 if (isset($_SESSION['id']) && isset($_SESSION['fname'])) {?>
-<!DOCTYPE html>
+<!DOCTYPE html lang="en">
 <html>
 <head>
 	<meta charset="utf-8">
@@ -31,6 +31,27 @@ if (isset($_SESSION['id']) && isset($_SESSION['fname'])) {?>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Edit Profile</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+           
 <div class="container-xl px-4 mt-4">
     <!-- Account page navigation-->
     <nav class="nav nav-borders">
@@ -76,15 +97,20 @@ if (isset($_SESSION['id']) && isset($_SESSION['fname'])) {?>
                             <label class="h3 mb-1">Email address: <?=$email?></label>
                         </div>
                         <!-- Save changes button-->
-                        <a class="btn btn-primary bt1 mb-2" href="#" role="button">Edit Profile</a>
+                        
+                        <a class="btn btn-primary bt1 mb-2" data-toggle="modal" data-target="#exampleModal" role="button" >Edit Profile</a>
                         <a class="btn btn-success " href="logout.php" role="button">LOG OUT</a>
                         
                     </form>
                 </div>
             </div>
         </div>
-        </div>
+        </div>     
 </div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </body>
 </html>

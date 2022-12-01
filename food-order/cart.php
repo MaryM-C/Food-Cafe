f315e37585bc2128d0533a9d60048ecc7b34b165
DanielLampa <jonaxxmontefalco7@gmail.com>
2022-12-02 01:13:09 +0800

    cart

--- food-order/cart.php
+++ food-order/cart.php
@@ -1,68 +1,72 @@
-<?php include('partials-front/menu.php');?>
 <?php 
-    // Check whether food id is set or not
-    if(isset($_GET['food_id'])) {
-        // get the food id and details of the selected food
-        $food_id = $_GET['food_id'];
-
-        //get the details of the selected food
-        $sql = "SELECT * FROM tbl_food WHERE id=$food_id";
-        $res = mysqli_query($conn, $sql);
-        $count = mysqli_num_rows($res);
-
-        // check whether the data is available or not
-        if($count ==1) {
-            $row = mysqli_fetch_assoc($res);
-
-            $title = $row['title'];
-            $price = $row['price'];
-            $image_name = $row['image_name'];
-        } else {
-            header('location:'.SITEURL);
-        }
-
-    } else {
-        header('location:'.SITEURL);
-    }
+session_start();
+include_once "config/db_connect.php";
+session_start();
+include_once "config/db_connect.php";
+$id=$_SESSION['id'];
+$sql = "SELECT * FROM `tbl_order` WHERE cust_id = ?";
+$result=$conn->prepare($sql);
+$result->execute([$id]);
 ?>
 
-    <!-- fOOD sEARCH Section Starts Here -->
-    <section class="food-search">
-        <div class="container">
+<!DOCTYPE html>
+<html lang="en">
+if (isset($_SESSION['id']) && isset($_SESSION['fname'])) {?>
+<!DOCTYPE html lang="en">
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cart</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cart</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+           
+<div class="container-xl px-4 mt-4">
+    <!-- Account page navigation-->
+    <nav class="nav nav-borders">
+        <a class="nav-link active ms-0 text-warning" href="index.php">Back to Home</a>
+    </nav>
+        <div class="table-responsive-sm">
+                    <table class="table">
+                    <thead class="thead-dark">
+                    <tr class="table-warning">
+                    <th scope="col" >Food</th>
+                    <th scope="col">Qty.</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Operations</th>
+                    </tr>
+                    </thead>
+                <tbody>
+                <p class="text-left h1 text-warning">MY CART</p>
+                    <?php 
+                        while ($row=$result->fetch()){
+                            $food = $row['food'];
+                            $qty=$row['qty'];
+                            $price=$row['price'];
+                            $status=$row['status'];
 
-            <h2 class="text-center text-yellow">Your Cart</h2>
-
-            <form action="" method="POST" class="order">
-                <fieldset>
-                    <legend><h3>Selected Food</h3></legend>
-
-                    <div class="food-menu-img">
-                        <?php 
-
-                            //check whether the image is available or not
-                            if($image_name =="") {
-                                //image not available 
-                                echo "<div class = 'error'>Image not available.</div>";
-                            } else {
-                                //image available 
-                                ?>
-                                    <img src="<?php echo SITEURL;?>images/food/<?php echo $image_name;?>" alt="<?php echo $title; ?>" class="img-responsive img-curve">
-                                <?php 
-
-                            }
-                        ?>
-
-                    </div>
-
-                    <div class="food-menu-desc">
-                        <h2><?php echo $title;?></h2>
-                        <input type = "hidden" name = "food" value = "<?php echo $title;?>">
-                        <p class="food-price2">₱ <?php echo $price;?></p>
-                        <input type = "hidden" name="price" value="<?php echo $price;?>"> <br>
-
-                        <div class="food-detail2"><h4>Quantity</h4></div> <br>
-                        <input type="number" name="qty" class="input-responsive" value="1" required>
-
-                    </div>
-
-                </fieldset>
+                            echo"
+                            <tr>
+                                <th class='text-white'>$food</th>
+                                <td class='text-white'>$qty</td>
+                                <td class='text-white'>$price</td>
+                                <td class='text-white'>$status</td>
+                                <td> <a class='btn btn-danger' href='#' role='button'>remove</a> </td>
+                            </tr>";
+        
+                        }
+                    
+                    ?>
+                        
+                </tbody>
+                </table> 
+        </div>
